bbb23d021c2a53eb536778b16f1e744362b223da
theboringschool <theboringschool.org@gmail.com>
2022-05-05 11:08:44 -0700

    Remote Access

---
+++ src/ToastNotify.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class ToastNotify extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>Hello, World</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ToastNotify;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/ToastNotify.js',
+  output: {
+    path: path.resolve('lib'),
+    filename: 'ToastNotify.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom      
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
